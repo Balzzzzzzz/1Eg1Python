8bab0eb50ce312ce1aca5f2c5cb26c7d115c952d
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2022-11-01 21:37:49 +0100

    Create Karta pracy 3b.py

---
+++ Karta pracy 3b.py
@@ -0,0 +1,67 @@
+Zad 1
+
+for i in range(1,32):
+ 
+ print(i)
+
+
+	Zad 2
+for i in range (0,1000):
+  
+  if i%2: 
+    
+   print(i**2)
+  else:
+  
+   print("")
+
+print("Pętla została zakonczona")
+
+	Zad 3
+for i in range (1000,100000):
+  
+ if i % 379: 
+    
+  print("", end="")
+  
+ else:
+    
+  print(i)
+  
+
+
+print("Pętla została zakonczona")
+
+	Zad 4
+
+for i in range (100,1000):
+  
+ if i % 5: 
+   
+   print("", end="")
+  
+ else:
+   
+   print(i)
+  
+ if i % 6: 
+    
+   print("", end="")
+  
+ else:
+    
+   print(i)
+  
+ if i % 11: 
+   
+   print("", end="")
+  
+ else:
+  
+   print(i)
+
+
+print("Pętla została zakonczona")
+
+
+	Zad 5
