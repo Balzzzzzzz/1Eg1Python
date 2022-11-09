e20e03e7ed2646d85727103008bc63d3cd41db70
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2022-11-09 14:44:26 +0100

    Update Karta pracy 3b.py

--- Karta pracy 3b.py
+++ Karta pracy 3b.py
@@ -78,3 +78,35 @@
 #  dla i w zakresie (2,k*2+2,2):
 # suma = suma + i
 # # drukuj(suma)
+
+# zad 7
+#m = int(input("Ilosc liczb: "))
+#suma = 0
+#for i in range(11, (m*2)+11, 2):
+#    if i<100:
+#      suma += i
+#print("Suma:", suma)
+
+# zad 8
+#w0 = int(input("Podaj kwote wejsciowa:"))
+#l = int(input("Ilosc lat:"))
+#kap = 0
+#wartosc = w0
+#for i in range(1, l+1):
+#    kap = wartosc * 0.06
+#    wartosc = wartosc + kap
+#print("Wartosc:", round(wartosc, 2))
+
+# zad 9
+#n = int(input("Ilosc liczb: "))
+#temp = 0
+#suma = 0
+#for i in range(n):
+#    temp = i*100+21
+#    suma += temp
+#print("Suma:", suma)
+
+# zad 10
+# for i in range(1,1001):
+#     if i%10==(i**0.5) or i%100==(i**0.5):
+#         print(i, end=" ")
