5efb35b2d6ac0761ca064708ac4ba1a97034fc62
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2022-11-13 21:11:02 +0100

    Update Karta pracy 3a.py

--- Karta pracy 3a.py
+++ Karta pracy 3a.py
@@ -42,12 +42,55 @@ for i in range(1, n+1):
          print()
          
  Zad 3
+  
+n = int(input())
+for i in range(1,n+1):
+    print("*", end="")
+    if i % 2 == 1:
+        print("|" * i, end="")
+    else:
+        print("-" * i, end="")
+print()
+    
+Zad 4
+  
+
          
          
+Zad 5         
 
+n = int(input())
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j==n//2+1:
+            print("*", end="")
+        elif i==n//2+1:
+            print("-", end="")
+        else:
+            print(" ", end="")
+    print()
+    if 5:
+        if 6:
+            if 6:
+                pass
+            else:
+                pass
+        else:
+            if 6:
+                pass
+         
 
+Zad 6         
 
-
+n = int(input())
+for i in range(n):
+    for j in range(n+1):
+        if j==i and not j==n//2:
+            print("*", end="")
+        elif j==n-i:
+            print("?", end="")
+        print(" ", end="")
+    print()
 
 
 
