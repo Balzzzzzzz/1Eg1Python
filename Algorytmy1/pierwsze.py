7414b84305cf58b8181f264a651ee8cc68c8183f
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2022-11-21 15:04:07 +0100

    Update pierwsze.py

--- Algorytmy1/pierwsze.py
+++ Algorytmy1/pierwsze.py
@@ -4,15 +4,336 @@
 #  liczba x ma swój dzielnik (o ile go ma) w przedziale [2, sqrt(x)]
 # Dzielnik właściwy - dielnik liczby oprócz 1 i niej samej
 
-n = int(input())
-for i in range(2, int( n ** 0,5) + 1):
-  if n % i == 0:
-    print("Liczba nie jest pierwsza")
-   exit()
-    print("liczba jest pierwsza")
+# n = int(input())
+# for i in range(2, int( n ** 0,5) + 1):
+#   if n % i == 0:
+#     print("Liczba nie jest pierwsza")
+#    exit()
+#     print("liczba jest pierwsza")
 
 #  2. Generator liczb pierwszych - liczby pierwsze w przedziale [p, q]
 # [10;15] -> 11 i 13
 # [20;22] -> Brak loiczb pierwszych
+#p, g = map(int, "771-1223-45-56"split())
 
-#  3. Generator liczb pierwszych - początkowe k liczb pierwszych
+# p,g = map(int, input().split())
+# for i in range (p,q + 1):
+#       flaga = True
+#       for j in range(2, int(i**0.5)+1):
+#            if i % j == 0:
+#                flaga = False
+#                break
+#                  if flaga:
+#                       pr int(i, end="")
+
+
+# #  3. Generator liczb pierwszych - początkowe k liczb pierwszych
+#  n = int(input("Podaj ile chcesz mieć liczb pierwszych: ") )
+#   x = 0:
+#   while 1:
+#     flaga = True
+#     for i in range ( 2, x )
+#     if x % 2 == 0:
+#       flaga = False
+#       brea
+#     if flaga:
+#       n = n - 1
+#       if n == 0:
+#         break
+# x = x+ 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
