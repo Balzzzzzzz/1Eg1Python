d90157245afbd0114adb96eac5b596b5572cc05c
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2022-11-28 15:06:16 +0100

    Create NWD notatka.py

---
+++ NWD notatka.py
@@ -0,0 +1,25 @@
+
+
+a,b = int(input()), int(input())
+while a != b:
+  if a > b : a = a - b
+  if b > a : b = b - a
+print(a)
+
+
+//MODULO = ???
+
+19 % 7 = 2 5/7 = 5
+
+19 % 18 = 1
+
+19 % 19 = 0
+
+19 % 20 = 19
+
+
+a,b = int(input()), int(input())
+while b > 0:
+  a,b = b , a % b
+print(a)
+
