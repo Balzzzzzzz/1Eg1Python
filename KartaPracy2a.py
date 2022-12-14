69e51454c989635919cfab5071dac93cf0a76ee6
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2022-12-14 14:37:52 +0100

    Update KartaPracy2a.py

--- KartaPracy2a.py
+++ KartaPracy2a.py
@@ -1,58 +1,66 @@
-# 1 (easy) Sprawdź czy suma dwóch wpisanych przez użytkownika jest liczbą parzystą.
-# Wejście: a, b
-# Wyjście: TAK / NIE
-
-a = int(input())
-b = int(input())
-  if (a+b) % 2 == 0:
-      print("suma tych liczb jest parzystą")
-  else:
-      print("suma tych liczb jest nie parzystą")
-  
-
-# 2. (easy) Sprawdź, czy średnia arytmetyczna dwóch wpisanych liczb jest wieksza od jej średniej geometrycz-nej.
-# Wejście: a,g
-# Wyjście: TAK / NIE
-  
-  a = int(input())
-g = int(input())
- if (a+g) / 2 > ( a*g ) ** 0.5:
-    print("Tak, jest większa")
- else:
-    print("Nie, nie jest większą")
-
-  
-#   3. (easy) Użytkownik podaje 3 liczby całkowite. Sprawdź czy dokładnie dwie z nich są sobie równe.
-#   Wejście: k,l,m
-#     Wyjście: TAK / NIE (wraz z podaniem tych dwóch równych o ile są takowe)
-      k = int(input())
-l = int(input())
-m = int(input())
-if (k==l and k==m and m==l):
-  print("TAK")
+# zad 1
+a, b = map(int, input("Wpisz dwie liczby przez spację: ").split())
+if (a + b) % 2 == 0:
+    print("suma jest liczbą parzystą")
 else:
-  print("NIE")
+    print("suma nie jest liczbą parzystą")
 
-  
-  
-#       4. (medium) Niech użytkownik wprowadzi 4 różne liczby całkowite. 
-#       Napisz program, który sprawdzi, któraz tych liczb jest najmniejsza.
-#       Wejście: a,b,c,d
-#         Wyjście: najmniejsza z nich
-          
-  
-  
-  
-  
-#           5. (medium) Sprawdź czy trzy wprowadzone przez użytkownika liczby spełniają nierówność trójkąta.
-#           Wejście: a, b, c
-#             Wyjście: TAK / NIE
+# zad 2
+a, g = map(int, input("podaj dwie dowolne liczby prez spację: ").split())
+if (a+g)/2 > (a*g) ** 0.5:
+    print("średnia arytmetyczna dwóch wpisanych liczb jest wieksza")
+else:
+    print("średnia arytmetyczna nie jest większa od średniej geomentrycznej")
 
+# zad 3
+k, l, m = map(int, input("Podaj 3 liczby całkowite: ").split())
+if k == l == m:
+    print("Wszystkie liczby są równe:", k)
+if (k == l) and not(k == l == m):
+    print("k = l =", k)
+if (k == m) and not(k == l == m):
+    print("k = m =", k)
+if (l == m) and not(k == l == m):
+    print("l = m =", l)
 
+zad 4
+print("Najmniejsza z podanych liczb:",min(map(int, input("Podaj 4 dowolne liczny całkowite: ").split())))
 
+a, b, c, d = map(int, input("podaj 4 dowolne liczny całkowite: ").split())
+min = a
+if min>b:
+    min=b
+if min>c:
+    min=c
+if min>d:
+    min=d
+print(min)
 
+# zad 5
+a, b, c = map(int, input("Podaj 3 długości boków trójkąta: ").split())
+if (a + b > c) and (a + c > b) and (b + c > a):
+    print("Tak, spełniają")
+else:
+    print("Nie, nie spełniają")
 
-#               6. (medium) Niech z trzech wprowadzonych przez użytkwnika liczbach da się zbudować trójąt. 
-#               Sprawdź czyten trójką jest ostrokątny, prostokątny czy rozwartokątny.
-#               Wejście: a, b, c
-#                 Wyjście: ostro-, prosto-, rozwarto-kątny
+# zad 6
+a, b, c = map(int, input("Wprowadź długości trzech boków trójkąta: ").split())
+if (a+b>c) and (b+c>a) and (a+c>b):
+    max = a
+    b1 = b
+    b2 = c
+    if max < b:
+        max = b
+        b1 = a
+    if max < c:
+        max = c
+        b1 = a
+        b2 = b
+    if (max**2) == (b1**2 + b2**2):
+        print("Trójkąt prostokątny")
+    if (max**2) < (b1**2 + b2**2):
+        print("trójkąt ostrokątny")
+    if (max**2) > (b1**2 + b2**2):
+        print("trójkąt rozwartokątny")
+else:
+    print("To nie jest trójkąt")
