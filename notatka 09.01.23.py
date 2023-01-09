9ca6fe8f6a5553cf151e803c5c0ee22b4eb19148
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2023-01-09 14:16:13 +0100

    Update notatka 09.01.23.py

--- notatka 09.01.23.py
+++ notatka 09.01.23.py
@@ -1,22 +1,24 @@
-from hashlib import md5
-print(md5("Baj Baj lolu").hexdigest())
-print(md5(b"nygus").hexdigest())
-print(md5(b"mygus").hexdigest())
-
-
-
-from hashlib import md5, sha1
-user = kamilek
-pass = nigeria321234
-print(md5("Baj Baj lolu").hexdigest())
-print(md5(b"nygus").hexdigest())
-print(md5(b"mygus").hexdigest())
-
-
-
-
-
-
+from hashlib import md5                      |  Biorę 2 najlepsze duże liczby pierwsze
+print(md5("Baj Baj lolu").hexdigest())       |  p = 13
+print(md5(b"nygus").hexdigest())             |  q = 11
+print(md5(b"mygus").hexdigest())             |  
+                                             |  szukam F = (p - 1) * (q-1) tzw funkcję Eulera
+                                             |  
+                                             |  Oblicz modul n = p * q
+from hashlib import md5, sha1                |
+user = kamilek                               |
+pass = nigeria321234                         |
+print(md5("Baj Baj lolu").hexdigest())       |
+print(md5(b"nygus").hexdigest())             |
+print(md5(b"mygus").hexdigest())             |
+                                             |
+NWD(a,b) = n                                 |
+                                             |
+                                             |
+x*a + y*b = NWD(a,b)                         |
+                                             |
+8*7 - 5*11 = NWD(7,11) = 1, x = 8 y = -5     |
+----------------------------------------------
 
 
 
