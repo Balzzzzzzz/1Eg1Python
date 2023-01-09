796ddb6c40ff847fda3f1b39811baa81295a0bf8
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2023-01-09 14:03:25 +0100

    Create notatka 09.01.23.py

---
+++ notatka 09.01.23.py
@@ -0,0 +1,32 @@
+from hashlib import md5
+print(md5("Baj Baj lolu").hexdigest())
+print(md5(b"nygus").hexdigest())
+print(md5(b"mygus").hexdigest())
+
+
+
+from hashlib import md5, sha1
+user = kamilek
+pass = nigeria321234
+print(md5("Baj Baj lolu").hexdigest())
+print(md5(b"nygus").hexdigest())
+print(md5(b"mygus").hexdigest())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
